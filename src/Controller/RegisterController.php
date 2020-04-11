11390dd3be8d28ffd845abaf54f78ea4995dd062
Victor <webmaster@regalonatural.com>
2020-04-11 11:00:24 +0200

    Commit inicial

---
+++ src/Controller/RegisterController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+//use http\Env\Request;
+use function Sodium\add;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\SubmitButton;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+class RegisterController extends AbstractController
+{
+    /**
+     * @Route("/register", name="register")
+     */
+    public function register(Request $request, UserPasswordEncoderInterface $passEncoder)
+    {
+        $form = $this->createFormBuilder()
+            ->add('username')
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'required' => true,
+                'first_options' => ['label' => 'Password'],
+                'second_options' => ['label' => 'Confirm Password']
+            ])
+            ->add('register', SubmitType::class, [
+                'attr' => ['btn btn-success float-right'
+                ]
+            ])
+            ->getForm();
+        $form->handleRequest($request);
+        if($form->isSubmitted()){
+            $data = $form->getData();
+
+            $user = new User();
+            $user->setUsername($data['username']);
+            $user->setPassword(
+                $passEncoder->encodePassword($user, $data['password'])
+            );
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('app_login'));
+
+        }
+        return $this->render('register/index.html.twig',[
+            'form' => $form->createView()
+        ]);
+
+    }
+}
